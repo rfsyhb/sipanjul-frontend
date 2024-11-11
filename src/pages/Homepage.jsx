5173459e318b00eee0b1fd50f64f093b33343592
Rafi Syihab <rafisyihab3@gmail.com>
2024-11-11 15:40:33 +0700

    update salescard and add new section for recenttransaction

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,41 @@
 import BestSellingCard from '../components/homepage/BestSellingCard';
+import RecentTransaction from '../components/homepage/RecentTransaction';
 import SalesCard from '../components/homepage/SalesCard';
+import {
+  dailySales,
+  monthlySales,
+  weeklySales,
+} from '../utils/dummyData';
 
 export default function Homepage() {
   return (
     <div className="flex flex-col h-full max-h-screen gap-2 pr-4">
-      <section className='flex flex-row gap-4'>
-        <SalesCard label="Hari" value={924000} oldValue={800000} />
-        <SalesCard label="Bulan" value={8928500} oldValue={10756000} />
-        <SalesCard label="Tahun" value={162893000} oldValue={154765900} />
+      <section className="flex flex-row gap-4">
+        <SalesCard
+          label="Hari"
+          value={dailySales.currentValue}
+          oldValue={dailySales.oldValue}
+          percentage={dailySales.percentage}
+          isNegative={dailySales.isNegative}
+        />
+        <SalesCard
+          label="Minggu"
+          value={weeklySales.currentValue}
+          oldValue={weeklySales.oldValue}
+          percentage={weeklySales.percentage}
+          isNegative={weeklySales.isNegative}
+        />
+        <SalesCard
+          label="Bulan"
+          value={monthlySales.currentValue}
+          oldValue={monthlySales.oldValue}
+          percentage={monthlySales.percentage}
+          isNegative={monthlySales.isNegative}
+        />
       </section>
       <BestSellingCard />
+
+      <RecentTransaction />
     </div>
   );
 }
