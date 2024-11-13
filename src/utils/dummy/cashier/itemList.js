f068c66071c89a73fd2d0f9084ac77e8f6ee9d43
Rafi Syihab <rafisyihab3@gmail.com>
2024-11-13 10:44:45 +0700

    add new items

--- src/utils/dummy/cashier/itemList.js
+++ src/utils/dummy/cashier/itemList.js
@@ -9,6 +9,15 @@ export const itemList = [
       'https://allofresh.id/media/catalog/product/cache/fb82878314ebdeb4be369a6c3c22b713/b/r/brc-8994209001796-1.jpg',
   },
   {
+    id: 'm-sphp-v',
+    name: 'Beras Medium SPHP Vietnam',
+    stock: 104,
+    packageSize: '5kg',
+    price: 63000,
+    imageUrl:
+      'https://allofresh.id/media/catalog/product/cache/fb82878314ebdeb4be369a6c3c22b713/b/r/brc-8994209001796-1.jpg',
+  },
+  {
     id: 'p-slyp',
     name: 'Beras Premium Slyp Super',
     stock: 60,
@@ -25,5 +34,50 @@ export const itemList = [
     price: 75000,
     imageUrl:
       'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/90/MTA-154604032/befood_befood_beras_prem_setra_ramos_5kg_full01_f2hb19pw.jpg',
+  },
+  {
+    id: 'siamk',
+    name: 'Beras Siam Kahayan',
+    stock: 64,
+    packageSize: '5kg',
+    price: 76000,
+    imageUrl:
+      'https://images.tokopedia.net/img/cache/700/VqbcmM/2024/3/19/d88395f7-bb8f-4971-aaa3-505c8dfda4e3.jpg.webp?ect=4g',
+  },
+  {
+    id: 'migorkita',
+    name: 'Minyak Goreng Kita',
+    stock: 18,
+    packageSize: '1lt',
+    price: 18000,
+    imageUrl:
+      'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/102/MTA-167180951/kita_kita_minyak_goreng_sawit_1_liter_full01_50669181.jpg',
+  },
+  {
+    id: 'bwgput',
+    name: 'Bawang Putih',
+    stock: 79,
+    packageSize: '500gr',
+    price: 19000,
+    imageUrl:
+      'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/99/MTA-137722580/ch_ch-bawang-putih-pack-500gr_full01.jpg',
+  },
+  {
+    id: 'bwgmer',
+    name: 'Bawang Merah',
+    stock: 98,
+    packageSize: '500gr',
+    price: 18000,
+    imageUrl:
+      'https://images.tokopedia.net/img/cache/700/hDjmkQ/2020/8/28/715f4ad8-2029-4d85-9de2-5a25c93e2884.jpg',
+  },
+  {
+    id: 'b-brng-t',
+    name: 'Beras Burung Tingang',
+    stock: 80,
+    packageSize: '5kg',
+    price: 76000,
+    imageUrl:
+      'https://images.tokopedia.net/img/cache/700/VqbcmM/2024/3/19/f3bf5113-e0ac-47b7-8e40-f47e82ff9489.jpg',
   },
 ];
