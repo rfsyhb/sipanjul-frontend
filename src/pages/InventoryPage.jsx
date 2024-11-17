efc01c1296d5a0ec4660053eedc5c997facef4b5
Rafi Syihab <rafisyihab3@gmail.com>
2024-11-17 14:29:03 +0700

    implement controlled input

--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -1,15 +1,30 @@
-import { itemList } from "../utils/dummyData";
-import InventoryItemCard from "../components/inventorypage/InventoryItemCard";
+import { itemList } from '../utils/dummyData';
+import InventoryItemCard from '../components/inventorypage/InventoryItemCard';
+import { useState } from 'react';
 
 export default function InventoryPage() {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearch = (e) => {
+    setSearchInput(e.target.value);
+  }
+
   return (
-    <div class="flex flex-row flex-wrap gap-4 justify-center">
-      {itemList.map((item) => (
-        <InventoryItemCard
-          key={item.id}
-          item={item}
-        />
-      ))}
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-row justify-between px-4">
+        <button
+          className="bg-actionBtn text-white p-2 rounded-md hover:bg-white hover:text-bg outline outline-actionBtn"
+          onClick={() => alert(searchInput)}
+        >
+          Tambah Barang
+        </button>
+        <input type="text" value={searchInput} onChange={handleSearch} className='px-2' />
+      </div>
+      <div className="flex flex-row flex-wrap gap-8 justify-center">
+        {itemList.map((item) => (
+          <InventoryItemCard key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
