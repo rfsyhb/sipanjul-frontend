f82c90eb49ae6772fb06456b90a797b431b0e343
Rafi Syihab <rafisyihab3@gmail.com>
2024-11-17 18:47:44 +0700

    add new item for GPM

---
+++ src/utils/dummy/gpm/gpmItem.js
@@ -0,0 +1,51 @@
+export const gpmItems = [
+  {
+    id: 1,
+    name: 'Beras SPHP',
+    price: 60000,
+    imageUrl:
+      'https://allofresh.id/media/catalog/product/cache/fb82878314ebdeb4be369a6c3c22b713/b/r/brc-8994209001796-1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Minyak Goreng Kita',
+    price: 17500,
+    imageUrl:
+      'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/102/MTA-167180951/kita_kita_minyak_goreng_sawit_1_liter_full01_50669181.jpg',
+  },
+  {
+    id: 3,
+    name: 'Gula Manis Kita',
+    price: 17500,
+    imageUrl:
+      'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//94/MTA-1614107/gula_pasir_putih_refill_1kg_full02.jpg',
+  },
+  {
+    id: 4,
+    name: 'Bawang Putih',
+    price: 18000,
+    imageUrl:
+      'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//94/MTA-1614107/gula_pasir_putih_refill_1kg_full02.jpg',
+  },
+  {
+    id: 5,
+    name: 'Bawang Merah',
+    price: 17000,
+    imageUrl:
+      'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//94/MTA-1614107/gula_pasir_putih_refill_1kg_full02.jpg',
+  },
+  {
+    id: 6,
+    name: 'Gula Polos',
+    price: 17000,
+    imageUrl:
+      'https://images.tokopedia.net/img/cache/700/VqbcmM/2021/6/26/328cc5af-d1db-4d93-8596-d8447027c8e8.jpg',
+  },
+  {
+    id: 7,
+    name: 'Minyak Goreng Biasa',
+    price: 17500,
+    imageUrl:
+      'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//93/MTA-40410685/siip_siip_minyak_goreng_-900_ml-_full02_on5f4euy.jpg',
+  },
+];
