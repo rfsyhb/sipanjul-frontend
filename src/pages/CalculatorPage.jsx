dbac1c753b5ecfca659a461d8d9cc24cf4148af7
Rafi Syihab <rafisyihab3@gmail.com>
2024-11-18 21:33:37 +0700

    add responsiveness for mobile phone

--- src/pages/CalculatorPage.jsx
+++ src/pages/CalculatorPage.jsx
@@ -6,11 +6,11 @@ export default function CalculatorPage() {
   const [items, setItems] = useState(initialGpmItemList);
   const [quantities, setQuantities] = useState(
     initialGpmItemList.reduce((acc, item) => {
-      acc[item.id] = 0;
+      acc[item.id] = null;
       return acc;
     }, {})
   );
-  const [cashGiven, setCashGiven] = useState(0);
+  const [cashGiven, setCashGiven] = useState("");
 
   const onChangePrice = (id, price) => {
     setItems((prevItems) =>
@@ -39,11 +39,11 @@ export default function CalculatorPage() {
     setQuantities(
       produce((draft) => {
         initialGpmItemList.forEach((item) => {
-          draft[item.id] = 0;
+          draft[item.id] = null;
         });
       })
     );
-    setCashGiven(0);
+    setCashGiven(null);
   };
 
   const calculateChange = () => {
@@ -51,30 +51,30 @@ export default function CalculatorPage() {
   };
 
   return (
-    <div className="flex flex-col items-center px-10 md:px-20 lg:px-32 gap-8">
+    <div className="flex flex-col items-center px-2 md:px-20 lg:px-32">
       {/* Kalkulator dan Daftar Produk */}
-      <div className="flex flex-col items-center gap-4 w-full max-w-4xl">
-        <h2 className="font-semibold text-2xl">
+      <div className="flex flex-col items-center gap-1 md:gap-4 w-full max-w-4xl">
+        <h2 className="font-semibold text-md md:text-2xl">
           Daftar Harga dan Kalkulator GPM
         </h2>
-        <div className="flex flex-col lg:flex-row gap-6 w-full">
+        <div className="flex flex-col lg:flex-row gap-1 md:gap-6 w-full overflow-y-auto h-[77vh] md:h-full">
           {/* Daftar Produk dengan qty input */}
-          <div className="flex flex-col flex-wrap gap-4 w-full lg:w-3/5">
+          <div className="flex flex-col flex-wrap gap-1 md:gap-4 w-full lg:w-3/5">
             {items.map((item) => (
               <div
                 key={item.id}
-                className="flex flex-row items-center gap-4 p-4 rounded-lg border border-gray-300 shadow-sm hover:shadow-md transition-shadow duration-300"
+                className="flex flex-row items-center gap-4 p-2 md:p-4 rounded-lg border border-gray-300 shadow-sm hover:shadow-md transition-shadow duration-300"
               >
                 <img
                   src={item.imageUrl}
                   alt={item.name}
-                  className="w-24 lg:w-12 object-cover rounded-lg overflow-hidden shadow-md border border-gray-200"
+                  className="w-12 object-cover rounded-lg overflow-hidden shadow-md border border-gray-200"
                 />
-                <div className="flex flex-col w-full lg:flex-row lg:justify-between lg:items-center">
+                <div className="flex w-full flex-col md:flex-row lg:justify-between lg:items-center">
                   <p className="font-medium text-lg w-full lg:w-1/2 truncate">
                     {item.name}
                   </p>
-                  <div className="flex flex-col lg:flex-row gap-2 w-full lg:w-1/2 justify-between ">
+                  <div className="flex flex-row gap-2 w-full lg:w-1/2 justify-between ">
                     <input
                       type="number"
                       value={item.price}
@@ -88,12 +88,13 @@ export default function CalculatorPage() {
                       <input
                         type="number"
                         id="quantity"
-                        value={quantities[item.id]}
+                        value={quantities[item.id] || ""}
                         onChange={(e) =>
                           onChangeQuantity(item.id, Number(e.target.value))
                         }
                         className="w-14 border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
                         min={0}
+                        placeholder="qty"
                       />
                     </div>
                   </div>
@@ -102,29 +103,32 @@ export default function CalculatorPage() {
             ))}
           </div>
           {/* Total price, cash input, change, and reset button */}
-          <div className="flex flex-col items-center justify-center lg:w-2/5 gap-4 p-4">
+          <div className="flex flex-col items-center justify-center lg:w-2/5 gap-1 md:gap-4 p-1 md:p-4">
             <div className="flex flex-row justify-between w-full">
-              <p className="text-lg font-bold">Total Harga</p>
-              <p className="text-2xl font-bold">
+              <p className="md:text-lg font-bold">Total Harga</p>
+              <p className="md:text-2xl font-bold">
                 Rp. {calculateTotalPrice().toLocaleString()}
               </p>
             </div>
             <div className="flex flex-row justify-between w-full items-center">
-              <label htmlFor="cashGiven" className="text-lg font-bold">
+              <label htmlFor="cashGiven" className="md:text-lg font-bold">
                 Uang Diterima:
               </label>
               <input
                 type="number"
                 id="cashGiven"
-                value={cashGiven}
+                value={cashGiven || ""}
                 onChange={(e) => setCashGiven(Number(e.target.value))}
                 className="w-1/2 border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
                 min={0}
+                placeholder="uang"
               />
             </div>
             <div className="flex flex-row justify-between w-full">
-              <p className="text-lg font-bold">Kembalian</p>
-              <p className={`text-xl font-bold ${cashGiven < calculateTotalPrice() ? 'text-red-500' : ''}`}>
+              <p className="md:text-lg font-bold">Kembalian</p>
+              <p
+                className={`md:text-xl font-bold ${cashGiven < calculateTotalPrice() ? 'text-red-500' : ''}`}
+              >
                 {cashGiven < calculateTotalPrice()
                   ? 'Uang kurang!!!'
                   : `Rp. ${calculateChange().toLocaleString()}`}
