17087d7f849a94985e33039427318d5e73120f9c
Rafi Syihab <rafisyihab3@gmail.com>
2024-12-05 16:39:38 +0700

    update for chart page

--- src/pages/ChartPage.jsx
+++ src/pages/ChartPage.jsx
@@ -10,6 +10,7 @@ import {
   Legend,
 } from 'chart.js';
 import { chartData } from '../utils/dummyData';
+import useIsMobile from '../hooks/useIsMobile';
 
 ChartJS.register(
   CategoryScale,
@@ -21,6 +22,7 @@ ChartJS.register(
 );
 
 const ChartPage = () => {
+  const isMobile = useIsMobile(768);
   // Function to generate chart data from dummy data
   const generateChartData = (periodData) => {
     return {
@@ -29,33 +31,56 @@ const ChartPage = () => {
         {
           label: `Total Penjualan (${periodData.period})`,
           data: periodData.data.map((item) => item.totalPenjualan),
-          backgroundColor: [
-            '#F0AB26',
-            '#1B4F88',
-          ],
+          backgroundColor: ['#F0AB26', '#1B4F88'],
         },
       ],
     };
   };
 
   return (
-    <div className="grid grid-cols-2 gap-5 h-[88vh] p-5 overflow-x-auto">
+    <div className="flex flex-col md:grid md:grid-cols-2 md:gap-5 lg:h-[88vh] p-4 overflow-y-auto h-[92vh]">
       {chartData.map((data, index) => (
-        <div key={index} className="p-5 bg-gray-100 rounded-lg shadow-md">
+        <div
+          key={index}
+          className="mb-3 md:mb-0 p-4 bg-gray-100 rounded-lg shadow-sm hover:shadow-md"
+          style={{ width: isMobile ? '99%' : '100%', height: isMobile ? '280px' : '400px' }} // Adjust height for better mobile experience
+        >
           <Bar
             data={generateChartData(data)}
             options={{
               responsive: true,
+              maintainAspectRatio: false, // Important for flexibility
               plugins: {
                 legend: {
                   position: 'top',
+                  labels: {
+                    font: {
+                      size: 12, // Adjust font size for smaller screens
+                    },
+                  },
                 },
                 title: {
                   display: true,
                   text: `PENJUALAN ${data.period.toUpperCase()}`,
                   font: {
-                    size: 16,
-                  }
+                    size: 14, // Adjust title font size
+                  },
+                },
+              },
+              scales: {
+                x: {
+                  ticks: {
+                    font: {
+                      size: 10, // Adjust font size for better readability
+                    },
+                  },
+                },
+                y: {
+                  ticks: {
+                    font: {
+                      size: 10,
+                    },
+                  },
                 },
               },
             }}
